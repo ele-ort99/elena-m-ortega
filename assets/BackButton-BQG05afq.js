9a12d29aacab9f2e5f7f87cf2486681bae58915e
Elena <elenaptlaboral@gmail.com>
2026-02-14 20:01:47 +0100

    Updates

---
+++ assets/BackButton-BQG05afq.js
@@ -0,0 +1,1 @@
+import{u as s,j as t}from"./index-DCJm0z1c.js";const o=()=>{const a=s();return t.jsx("div",{className:"aboutActions",children:t.jsx("button",{type:"button",onClick:()=>a("/"),className:"aboutBackBtn",children:"VOLVER"})})};export{o as B};
