4c57a92a1f55684cb8d2183de7b8705b6a79a5da
Elena <elenaptlaboral@gmail.com>
2026-02-14 19:30:18 +0100

    Updates

--- assets/BackButton-C7_npSji.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as t}from"./index-DF6_NExg.js";const o=()=>{const a=s();return t.jsx("div",{className:"aboutActions",children:t.jsx("button",{type:"button",onClick:()=>a("/"),className:"aboutBackBtn",children:"VOLVER"})})};export{o as B};
